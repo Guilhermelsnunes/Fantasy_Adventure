a737cf9beabd4623be4e05ac44b6d2af52528cff
Guilherme Nunes <guilhermelsnunes@gmail.com>
2022-03-08 17:40:43 +0000

    problems warlock wizard

--- src/main/java/player/Warlock.java
+++ src/main/java/player/Warlock.java
@@ -1,10 +1,10 @@
 package player;
 
 import magic.ISpell;
-
-public class Warlock extends Mage{
-
-    public Warlock(String name, int healthPoints, ISpell spell) {
-        super(name, healthPoints, spell);
-    }
-}
+//
+//public class Warlock extends Mage{
+//
+//    public Warlock(String name, int healthPoints, ISpell spell) {
+//        super(name, healthPoints, spell);
+//    }
+//}
--- src/main/java/player/Wizard.java
+++ src/main/java/player/Wizard.java
@@ -1,11 +1,11 @@
-package player;
-
-import magic.ISpell;
-
-public class Wizard extends Mage {
-
-    public Wizard(String name, int healthPoints, ISpell spell) {
-        super(name, healthPoints, spell);
-    }
-
-}
+//package player;
+//
+//import magic.ISpell;
+//
+//public class Wizard extends Mage {
+//
+//    public Wizard(String name, int healthPoints, ISpell spell) {
+//       super(name, healthPoints, spell);
+//    }
+//
+//}
